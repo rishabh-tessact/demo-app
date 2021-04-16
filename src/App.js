0ee29a174d8a11c5c57feffb9ac55a9fddd30c07
Rishabh Das <rishabhdasufl@gmail.com>
2021-04-16 14:03:00 +0530

    cat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-//import CacheBuster from "./CacheBuster";
-// import SignIn from "./SignIn";
+import CacheBuster from "./CacheBuster";
+import SignIn from "./SignIn";
 import "./App.css";
 
 var CACHE_NAME = "my-cache_name";
@@ -18,28 +18,27 @@ class App extends Component {
   }
   render() {
     return (
-      // <CacheBuster>
-      //   {({ loading, isLatestVersion, refreshCacheAndReload }) => {
-      //     if (loading) return null;
-      //     if (!loading && !isLatestVersion) {
-      //       refreshCacheAndReload();
-      //     }
+      <CacheBuster>
+        {({ loading, isLatestVersion, refreshCacheAndReload }) => {
+          if (loading) return null;
+          if (!loading && !isLatestVersion) {
+            refreshCacheAndReload();
+          }
 
-      //     return (
-      //       <div className="App">
-      //         <header className="App-header">
-      //           <h1>Cache Busting - Example</h1>
-      //           <p>
-      //             Bundle version - <code>v{global.appVersion}</code>
-      //           </p>
-      //         </header>
-      //         Form Added
-      //         <SignIn />
-      //       </div>
-      //     );
-      //   }}
-      // </CacheBuster>
-      <h1>app load</h1>
+          return (
+            <div className="App">
+              <header className="App-header">
+                <h1>Cache Busting - Example</h1>
+                <p>
+                  Bundle version - <code>v{global.appVersion}</code>
+                </p>
+              </header>
+              Form Added
+              <SignIn />
+            </div>
+          );
+        }}
+      </CacheBuster>
     );
   }
 }
