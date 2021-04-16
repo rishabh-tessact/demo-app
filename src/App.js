1e5d57bbd5c1a4d2f19c236f0e6c6ef7dbde32d1
Rishabh Das <rishabhdasufl@gmail.com>
2021-04-16 13:55:38 +0530

    cat

--- src/App.js
+++ src/App.js
@@ -18,27 +18,28 @@ class App extends Component {
   }
   render() {
     return (
-      <CacheBuster>
-        {({ loading, isLatestVersion, refreshCacheAndReload }) => {
-          if (loading) return null;
-          if (!loading && !isLatestVersion) {
-            refreshCacheAndReload();
-          }
+      // <CacheBuster>
+      //   {({ loading, isLatestVersion, refreshCacheAndReload }) => {
+      //     if (loading) return null;
+      //     if (!loading && !isLatestVersion) {
+      //       refreshCacheAndReload();
+      //     }
 
-          return (
-            <div className="App">
-              <header className="App-header">
-                <h1>Cache Busting - Example</h1>
-                <p>
-                  Bundle version - <code>v{global.appVersion}</code>
-                </p>
-              </header>
-              Form Added
-              <SignIn />
-            </div>
-          );
-        }}
-      </CacheBuster>
+      //     return (
+      //       <div className="App">
+      //         <header className="App-header">
+      //           <h1>Cache Busting - Example</h1>
+      //           <p>
+      //             Bundle version - <code>v{global.appVersion}</code>
+      //           </p>
+      //         </header>
+      //         Form Added
+      //         <SignIn />
+      //       </div>
+      //     );
+      //   }}
+      // </CacheBuster>
+      <h1>app load</h1>
     );
   }
 }
