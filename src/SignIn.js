4b16582a5a1a84895644aeb457bf0fc7d936872b
Rishabh Das <rishabhdasufl@gmail.com>
2021-04-16 13:09:42 +0530

    dede

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,41 +1,41 @@
-import React from "react"
+import React from "react";
 
 export default class SignIn extends React.Component {
-    state = {
-      user: '',
-      rememberMe: false
-    };
+  state = {
+    user: "",
+    rememberMe: false,
+  };
 
-    componentDidMount() {
-        const rememberMe = localStorage.getItem('rememberMe') === 'true';
-        const user = rememberMe ? localStorage.getItem('user') : '';
-        this.setState({ user, rememberMe });
-    }
-   
-    handleChange = (event) => {
-      const input = event.target;
-      const value = input.type === 'checkbox' ? input.checked : input.value;
-   
-      this.setState({ [input.name]: value });
-    };
-   
-    handleFormSubmit = () => {
-        const { user, rememberMe } = this.state;
-        localStorage.setItem('rememberMe', rememberMe);
-        localStorage.setItem('user', rememberMe ? user : '');
-    };
-   
-    render() {
-        return (
-            <form onSubmit={this.handleFormSubmit}>
-              <label>
-                User: <input name="user" value={this.state.user} onChange={this.handleChange}/>
-              </label>
-              <label>
-                <input name="rememberMe" checked={this.state.rememberMe} onChange={this.handleChange} type="checkbox"/> Remember me
-              </label>
-              <button type="submit">Sign In</button>
-            </form>
-          );
-    }
+  componentDidMount() {
+    const rememberMe = localStorage.getItem("rememberMe") === "true";
+    const user = rememberMe ? localStorage.getItem("user") : "";
+    this.setState({ user, rememberMe });
   }
+
+  handleChange = (event) => {
+    const input = event.target;
+    const value = input.type === "checkbox" ? input.checked : input.value;
+
+    this.setState({ [input.name]: value });
+  };
+
+  handleFormSubmit = () => {
+    const { user, rememberMe } = this.state;
+    localStorage.setItem("rememberMe", rememberMe);
+    localStorage.setItem("user", rememberMe ? user : "");
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <label>
+          User: <input name="user" value={this.state.user} onChange={this.handleChange} />
+        </label>
+        <label>
+          <input name="rememberMe" checked={this.state.rememberMe} onChange={this.handleChange} type="checkbox" /> Remember me
+        </label>
+        <button type="submit">Sign In</button>
+      </form>
+    );
+  }
+}
