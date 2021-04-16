29241467f543f34a8bf9df5c0fddf69cf6596fbf
Rishabh Das <rishabhdasufl@gmail.com>
2021-04-16 13:28:43 +0530

    cat

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import CacheBuster from "./CacheBuster";
 import SignIn from "./SignIn";
 import "./App.css";
 
-var CACHE_NAME = 'my-cache_name';
-var targetsToCache = [
-  '/styles/myStyles.scss',
-  'www.stackoverflow.com/'
-];
+var CACHE_NAME = "my-cache_name";
+var targetsToCache = ["/styles/myStyles.scss", "www.stackoverflow.com/"];
 
-document.addEventListener('install', function(event) {
+document.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(targetsToCache);
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(targetsToCache);
+    })
   );
 });
 class App extends Component {
-
-
-
   render() {
     return (
       <CacheBuster>
